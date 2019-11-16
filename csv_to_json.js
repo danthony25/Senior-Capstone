bbfa1409fc1671a7307b64ac83a9750791ca97dd
danthony25 <54610907+danthony25@users.noreply.github.com>
2019-11-15 17:13:22 -0700

    Update csv_to_json.js

--- csv_to_json.js
+++ csv_to_json.js
@@ -1,6 +1,23 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const express = require("express");
+const app = express();
 var data = []; 
+app.use(express.static('public'));
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(data)
+
+})
+
 fs.createReadStream('charities_updated_2.csv')
   .pipe(csv())
   .on('data', (row) => {
@@ -10,5 +27,6 @@ fs.createReadStream('charities_updated_2.csv')
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
-    console.log(data)
+    // res.send(data[0].Mission)
   });
+app.listen(3000);
