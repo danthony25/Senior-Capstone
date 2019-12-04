afd83bb0a58a8198307deac4a59b8392d8dd96be
aleahmcrawford <49804895+aleahmcrawford@users.noreply.github.com>
2019-12-04 13:32:12 -0700

    Add files via upload

---
+++ Final/app.js
@@ -0,0 +1,33 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const express = require("express");
+const app = express();
+var data = []; 
+app.use(express.static('public'));
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(data)
+  console.log(data)
+
+})
+
+fs.createReadStream('charities_updated_2.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    // console.log(row);
+    data.push(row)
+    
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+    // res.send(data[0].Mission)
+  });
+app.listen(3000);
